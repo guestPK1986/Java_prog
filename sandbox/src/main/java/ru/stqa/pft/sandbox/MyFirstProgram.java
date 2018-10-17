c675078834e0237ba63d97d20a9653c33aa6f4e1
polina <kps090111@gmail.com>
2018-10-17 14:30:59 -0500

    added double functions to the main class

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,29 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main (String[] args) {
-	
-	System.out.println("Hello, world");
+
+		hello("world");
+
+		double len = 5;
+		System.out.println("Square of square with side " + len + " = " + area(len));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Square of rectangle with sides " + a + " and " + b + " = " + area(a,b));
 	}
 
+
+	public static void hello (String something){
+		System.out.println("Hello " + something + "!");
+	}
+
+	public static double area (double l){
+
+		return l*l;
+	}
+
+	public static double area (double a, double b){
+
+		return a*b;
+	}
 }
