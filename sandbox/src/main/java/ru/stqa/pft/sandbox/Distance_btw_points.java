2397625557f355e47a58b8b9e2f1ec6ffb8794a0
polina <kps090111@gmail.com>
2018-10-20 00:11:27 -0500

    updated classes point and distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance_btw_points.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance_btw_points.java
@@ -1,20 +1,17 @@
 package ru.stqa.pft.sandbox;
+import org.testng.annotations.Test;
+
 
 public class Distance_btw_points{
 
+  @Test
   public static void main(String[] args) {
-    Point p1 = new Point();
-    p1.x1 = 7;
-    p1.y1 = 3;
+    Point p1 = new Point(7,4);
+    Point p2 = new Point(5,4);
 
-    Point p2 = new Point();
-    p2.x2 = 5;
-    p2.y2 = 4;
+    double d = p1.Distance(p2);
 
-    System.out.println("Distance between points is " + distance(p1, p2));
-  }
+    System.out.println("Distance between points is " + d);
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x2 - p1.x1) * (p2.x2 - p1.x1) + (p2.y2 - p1.y1) * (p2.y2 - p1.y1));
-  }
 }
+
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,21 +1,33 @@
 package ru.stqa.pft.sandbox;
 
+import java.awt.*;
+
 public class Point {
 
-  public double x1;
-  public double y1;
-  public double x2;
-  public double y2;
+  public double x;
+  public double y;
+  private Event p2;
+  private Event p1;
 
+
+  public Point (double x, double y){
+    this.x = x;
+    this.y = y;
+
+  }
+
+  public double Distance() {
+
+    //return Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 - this.y1));
+
+    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+
+
+  }
 }
 
 
 
-
-
-
-
-    //double sqrt = Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 - this.y1));
 
 
 
